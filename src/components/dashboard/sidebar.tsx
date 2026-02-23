292fe4f45773c531889873f85b193e62a5740d77
Karl Mc Jay Valmores <valmores.karlmcjay@gmail.com>
2026-02-23 08:29:03 +0800

    Backup-Karl

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -207,7 +207,7 @@ export default function AdminSidebar() {
                     </SidebarMenuItem>
 
                 </SidebarMenu>
-            </SidebarFooter>
+            </SidebarFooter>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
         </Sidebar>
     )
 }

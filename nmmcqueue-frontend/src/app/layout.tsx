ea08d49a31607e9fd0208ee1d858e515d383b75a
aljo nicolo <andina.aljo@gmail.com>
2026-03-04 15:57:22 +0800

    font change and added readme

--- nmmcqueue-frontend/src/app/layout.tsx
+++ nmmcqueue-frontend/src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Queueing System: NMMC",
@@ -21,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body
 
-        className={`${poppins.variable} ${inter.variable} font-sans text-black antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans text-black antialiased`}
       >
         {children}
       </body>
